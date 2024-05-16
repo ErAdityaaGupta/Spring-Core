6d9a63840cd064325bf7350b2f7c58a836c561f0
Aditya Gupta <ingenious.adig@gmail.com>
2024-05-16 19:16:34 +0530

    Initial commit

---
+++ src/main/java/com/demo/springcoredemo/DemoController.java
@@ -0,0 +1,27 @@
+package com.demo.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for thr dependency
+    private Coach myCoach;
+
+    // define the constructor for dependency injection.
+    @Autowired
+    public DemoController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+
+
+}
