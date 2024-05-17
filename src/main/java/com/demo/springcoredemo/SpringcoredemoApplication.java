8b55ce72c1431525417698c55946a11b081f598d
Aditya Gupta <ingenious.adig@gmail.com>
2024-05-17 23:57:24 +0530

    Initial commit

--- src/main/java/com/demo/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/demo/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,9 @@ package com.demo.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"com.demo.springcoredemo","com.demo.util"}
+)
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
