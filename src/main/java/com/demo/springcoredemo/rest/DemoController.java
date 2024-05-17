443eafff736d9cea7f9f01d017af8983d9d37494
Aditya Gupta <ingenious.adig@gmail.com>
2024-05-17 23:57:14 +0530

    Initial commit

--- src/main/java/com/demo/springcoredemo/rest/DemoController.java
+++ src/main/java/com/demo/springcoredemo/rest/DemoController.java
@@ -1,6 +1,6 @@
 package com.demo.springcoredemo.rest;
 
-import com.demo.springcoredemo.common.Coach;
+import com.demo.util.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
