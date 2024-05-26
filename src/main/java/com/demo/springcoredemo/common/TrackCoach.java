e1cac9029b3a3cf86bacb6a19b4537e5e5fb1f18
Aditya Gupta <ingenious.adig@gmail.com>
2024-05-26 23:59:56 +0530

    Initial commit

--- src/main/java/com/demo/springcoredemo/common/TrackCoach.java
+++ src/main/java/com/demo/springcoredemo/common/TrackCoach.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 //@Primary
 public class TrackCoach implements Coach{
 
+    public TrackCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice running for 30 minutes.";
