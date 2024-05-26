837a03bd3db846e2f4d10d9a3463d9bcf0a7d57e
Aditya Gupta <ingenious.adig@gmail.com>
2024-05-27 03:53:32 +0530

    Initial commit

--- src/main/java/com/demo/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/demo/springcoredemo/common/TennisCoach.java
@@ -1,8 +1,10 @@
 package com.demo.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class TennisCoach implements Coach{
 
     public TennisCoach(){
