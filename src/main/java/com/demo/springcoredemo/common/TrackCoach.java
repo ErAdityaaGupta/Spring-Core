8496424bfc91405bcbc7f26df6986d75d71e1f6b
Aditya Gupta <ingenious.adig@gmail.com>
2024-05-27 22:27:45 +0530

    Initial commit

--- src/main/java/com/demo/springcoredemo/common/TrackCoach.java
+++ src/main/java/com/demo/springcoredemo/common/TrackCoach.java
@@ -1,5 +1,7 @@
 package com.demo.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
@@ -13,6 +15,18 @@ public class TrackCoach implements Coach{
 
     public TrackCoach(){
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupWork(){
+        System.out.println("In doMyStartupWork method." + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff method" + getClass().getSimpleName());
     }
 
     @Override
